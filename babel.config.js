d82c06b2a121a6d3c1e8854933ccf1a6427df56c
devparkoon <jh.park@qualson.com>
2020-01-07 21:30:32 +0900

    hot module reloading.... fail

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    presets: ['@babel/preset-env', '@babel/react'],
+    plugins: [
+        // '@babel/transform-runtime',
+        '@babel/proposal-class-properties'
+        // 'react-hot-loader/babel'
+    ]
+}
--- src/client/components/layout/Header/Header.js
+++ src/client/components/layout/Header/Header.js
@@ -1,25 +1,25 @@
-import React from "react";
-import classNames from "classnames/bind";
-import { Link } from "react-router-dom";
-import styles from "./header.scss";
+import React from 'react'
+import classNames from 'classnames/bind'
+import { Link } from 'react-router-dom'
+import styles from './header.scss'
 
-const cx = classNames.bind(styles);
+const cx = classNames.bind(styles)
 
 const Header = () => {
-  return (
-    <header className={cx("header")}>
-      <div className={cx("header__logo")}>
-        <Link to="/">SSR Bolierplate</Link>
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/users">API</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
+    return (
+        <header className={cx('header')}>
+            <div className={cx('header__logo')}>
+                <Link to="/">SSR Bolierplate!!!</Link>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/users">API</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
 
-export default Header;
+export default Header
